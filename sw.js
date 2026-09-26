21487e8f599f4a835dd1ddaf355fbc6fdf4b4465
akbarhussaincs3-blip <akbarhussaincs3@gmail.com>
2026-09-26 10:39:17 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,9 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   self.clients.claim();
 });
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
